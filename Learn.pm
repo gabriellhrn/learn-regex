79f0952e285370dd7fcf2d4016a579bec769ba60
gabriell <gabriellhrn@gmail.com>
2016-01-31 21:00:07 -0200

    Initializing repository structure

---
+++ Learn.pm
@@ -0,0 +1,30 @@
+#!/usr/bin/perl 
+
+package Learn;
+
+use strict;
+use warnings;
+
+sub parse {
+    my $c = loadfile("Learn.txt");
+
+    ##
+    # write your code below
+
+    ##
+
+    $c;
+}
+
+sub loadfile {
+    my $file = $_[0];
+    open(my $fh, "<", $file);
+
+    my $c;
+    while(<$fh>) { $c .= $_; }
+
+    $c;
+}
+
+1;
+
---
+++ README.md
@@ -0,0 +1,3 @@
+
+A description will be added
+
